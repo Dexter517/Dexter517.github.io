c65f51b58627e96b047b40d36f54f2f68c981c99
Dexter517 <adg6339902@lpssonline.com>
2024-10-07 16:07:09 +0000

    added platforms and collectibles to game

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -1,8 +1,9 @@
-$(function 
+$(function () {
   // initialize canvas and context when able to
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
   window.addEventListener("load", loadJson);
+
 
   function setup() {
     if (firstTimeSetup) {
@@ -21,6 +22,7 @@ $(function
     createPlatform(-50, -50, 50, canvas.height + 500); //bottom
     createPlatform(canvas.width, -50, 50, canvas.height + 100);
 
+
     /**
      * Uncomment the drawGrid() function call below to add a "grid" to your platformer game's screen
      * The grid will place both horizontal and vertical platforms incremented 100 pixels apart
@@ -28,42 +30,55 @@ $(function
      * Comment the function call out to remove the grid
      */
 
-    // drawGrid();
+
+    drawGrid();
+
 
     /////////////////////////////////////////////////
     //////////ONLY CHANGE BELOW THIS POINT///////////
     /////////////////////////////////////////////////
+
 
     // TODO 1
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
     createPlatform(500, 300, 200, 20)
-    createPlatform(400, 600, 200, 20)
+    createPlatform(400, 650, 200, 20)
+    createPlatform(700, 550, 200, 20)
+    createPlatform(900, 420, 200, 20)
 
-    
+   
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
+    createCollectable("steve", 500, 100, 20, 0.5)
+    createCollectable("grace", 500, 200)
+    createCollectable("max", 1000, 200)
 
 
-
-    
+   
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
+    createCannon("left", 450, 1000)
+    createCannon("right", 350, 1000)
+    createCannon("top", 800, 1000)
 
 
-
-    
+   
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
   }
 
+
   registerSetup(setup);
 });
+
+
+
